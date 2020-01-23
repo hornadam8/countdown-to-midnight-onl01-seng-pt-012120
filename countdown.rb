cf0a215ec89209aaed3717989527c97824c79feb
Adam G Horn <hornadam8@gmail.com>
2020-01-23 20:17:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown(number)
   
   return "HAPPY NEW YEAR!"
 end
+
+def countdoown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECONDS!"
+    number -= 1
+    sleep 1
+  end
+  
+  return "HAPPY NEW YEAR!"
+end
